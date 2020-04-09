7c67ab595179f8ff0b0ed3301092dd1dc85c52c6
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-04-09 13:50:13 +0800

    - just chess created

---
+++ Just Chess/Just Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Just_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
